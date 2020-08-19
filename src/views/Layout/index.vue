3a9c982b3d052e147ce05ee18ee0e1c1c31d66c9
jax2046 <jax2046@outlook.com>
2020-08-19 12:39:06 +0800

    token处理

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -6,12 +6,12 @@
       <layout-nav/>
     </aside>
     <section class="main">
-        <header>
-          <layout-header></layout-header>
-        </header>
-        <section class="content">
-          <Layout-content></Layout-content>
-        </section>
+      <header>
+        <layout-header></layout-header>
+      </header>
+      <section class="content">
+        <Layout-content></Layout-content>
+      </section>
     </section>
   </div>
 </template>
@@ -19,7 +19,8 @@
 import LayoutNav from './Components/Nav'
 import LayoutHeader from './Components/Header'
 import LayoutContent from './Components/Content'
-import {ref,reactive,computed} from '@vue/composition-api'
+import { ref, reactive, computed } from '@vue/composition-api'
+
 export default {
 	name: 'layout',
 	components: {
@@ -27,37 +28,44 @@ export default {
 		LayoutHeader,
 		LayoutContent
 	},
-	setup (props,{root}) {
-		const menuStatus = computed(()=>root.$store.state.app.isCollapse)
-    return {
+	setup (props, { root }) {
+		const menuStatus = computed(() => root.$store.state.app.isCollapse)
+		return {
 			menuStatus
-    }
+		}
 	}
 }
 </script>
 <style scoped lang="scss">
 @import "../../assets/styles/variable";
+
 #layout-wrap {
   display: flex;
-  &.close>aside{
+  
+  &.close > aside {
     width: 64px !important;
   }
-  &.open>section{
+  
+  &.open > section {
     width: 250px !important;
   }
+  
   aside {
     width: 250px;
     background-color: $theme-color;
     height: 100vh;
     transition: width .5s linear;
   }
+  
   section.main {
     flex: 1;
     display: flex;
     flex-direction: column;
+    
     header {
       height: 75px;
     }
+    
     section.content {
       flex: 1;
       border-top: 30px solid #f7f7f7;
